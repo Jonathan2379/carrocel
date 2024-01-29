1d09783f9c896e7f88a6929be6a86a5f8364c68d
Jonathan Oliveira <jonathan-boy1999@hotmail.com>
2024-01-29 00:53:27 -0400

    add

---
+++ slides.js
@@ -0,0 +1,20 @@
+const data = [
+    {
+        id: '1',
+        title: 'JavaScript',
+        description: 'eu sou o JavaScript',
+        image: require('./assets/JavaScript.svg.png')
+    },
+    {
+        id: '2',
+        title: 'TypeScript',
+        description: 'eu sou o TypeScript',
+        image: require('./assets/Typescript.svg.png')
+    },
+    {
+        id: '3',
+        title: 'Prisma',
+        description: 'eu sou o Prisma',
+        image: require('./assets/prisma.png')
+    }
+];
